34d1f883359dc4d802f4ecdeaafee08c2dde94a6
Ross Fenning <ross.fenning@gmail.com>
2013-09-21 01:40:06 +0100

    Refactored to serve both HTML and JSON requests.

--- doyoulikeit/doyoulikeit.py
+++ doyoulikeit/doyoulikeit.py
@@ -2,19 +2,36 @@ from kule import Kule, jsonify
 from bottle import abort, response
 from SPARQLWrapper import SPARQLWrapper, JSON
 import json
+from bottle.ext.pystache import view
+import mimerender
+import pystache
+import os
+
+
+render_html = lambda **args: pystache.render(
+    open(os.path.join(os.path.dirname(__file__), 'thing.mustache')).read(),
+    dict(args))
+render_json = lambda **args: json.dumps(args)
+
+mimerender = mimerender.BottleMimeRender()
 
 
 class DoYouLikeIt(Kule):
     def get_detail(self, collection, pk):
         return getattr(self, 'get_%s_detail' % collection)(pk)
 
+    @mimerender(
+        default='html',
+        html=render_html,
+        json=render_json,
+        )
     def get_things_detail(self, pk):
         cursor = self.get_collection('things')
         data = cursor.find_one({"thing_id": pk}
             ) or self.fetch_remote(pk) or abort(404)
 
         del data['_id']
-        return jsonify(data)
+        return data
 
     def fetch_remote(self, thing_id):
         sparql = SPARQLWrapper("http://dbpedia.org/sparql")
@@ -44,6 +61,13 @@ WHERE {
 
         return new_thing
 
+    def after_request(self):
+        """A bottle hook for json responses."""
+        methods = 'PUT, PATCH, GET, POST, DELETE, OPTIONS'
+        headers = 'Origin, Accept, Content-Type'
+        response.headers['Access-Control-Allow-Origin'] = '*'
+        response.headers['Access-Control-Allow-Methods'] = methods
+        response.headers['Access-Control-Allow-Headers'] = headers
 
 if __name__ == "__main__":
     DoYouLikeIt(database="doyoulikeit").run()
--- features/step_definitions/request.py
+++ features/step_definitions/request.py
@@ -4,4 +4,5 @@ import json
 
 @step(u'When I GET "([^"]*)"')
 def when_i_get_group1(step, path):
-    world.response = world.app.get(path).json
+    world.response = world.app.get(
+        path, headers={'Accept': 'application/json'}).json
