926031888526749f663fb0bd0ebb080cb4457737
Ross Fenning <ross.fenning@bridge-u.com>
2025-04-23 23:10:47 +0100

    Reduce the influence of signifance and make more random

--- doyoulikeit/main.py
+++ doyoulikeit/main.py
@@ -3,9 +3,18 @@ import os
 from urllib.parse import urlencode, quote_plus
 import pydash
 
-from authlib.integrations.flask_oauth2 import ResourceProtector
 from jinja2_fragments.quart import render_block
-from quart import Quart, g, render_template, redirect, make_response, request, Response, url_for, session
+from quart import (
+    Quart,
+    g,
+    render_template,
+    redirect,
+    make_response,
+    request,
+    Response,
+    url_for,
+    session,
+)
 from quart_authlib import OAuth
 from quart_db import QuartDB
 from typing_extensions import TypedDict
@@ -16,7 +25,7 @@ app: Quart = Quart(__name__, template_folder="templates")
 app.config["SECRET_KEY"] = os.environ["SECRET_KEY"]
 db: QuartDB = QuartDB(app, url=os.getenv("DATABASE_URL", "sqlite:memory:"))
 wikidata: Client = Client()
-image_prop = wikidata.get(EntityId('P18'))
+image_prop = wikidata.get(EntityId("P18"))
 
 oauth = OAuth(app)
 oauth.register(
@@ -26,13 +35,13 @@ oauth.register(
     client_kwargs={
         "scope": "openid profile email",
     },
-    server_metadata_url=f'https://{os.getenv("AUTH0_DOMAIN")}/.well-known/openid-configuration'
+    server_metadata_url=f'https://{os.getenv("AUTH0_DOMAIN")}/.well-known/openid-configuration',
 )
 
 
 @app.route("/login")
 async def login():
-    redirect_uri = url_for('callback', _external=True)
+    redirect_uri = url_for("callback", _external=True)
     return oauth.auth0.authorize_redirect(redirect_uri)
 
 
@@ -47,7 +56,8 @@ async def callback():
 def logout():
     session.clear()
     return redirect(
-        "https://" + os.getenv("AUTH0_DOMAIN")
+        "https://"
+        + os.getenv("AUTH0_DOMAIN")
         + "/v2/logout?"
         + urlencode(
             {
@@ -59,7 +69,7 @@ def logout():
     )
 
 
-@app.route('/')
+@app.route("/")
 async def home():
     user_id = pydash.get(session, "user.userinfo.sub")
     new_thing = await random_thing(user_id)
@@ -76,36 +86,44 @@ class Thing(TypedDict):
 
 async def random_thing(user_id: str | None) -> str | None:
     if user_id:
-        result = await g.connection.fetch_one("""
-        SELECT t.thing_id 
+        result = await g.connection.fetch_one(
+            """
+        SELECT t.thing_id
         FROM things t
         LEFT JOIN votes v
         ON t.thing_id = v.thing_id AND v.user_id = :user_id
         WHERE v.thing_id IS NULL
-        ORDER BY RANDOM() * t.significance DESC
+        ORDER BY RANDOM() * LOG(t.significance + 1) DESC
         LIMIT 1;
-        """, {"user_id": user_id})
+        """,
+            {"user_id": user_id},
+        )
     else:
         result = await g.connection.fetch_one("""
-        SELECT t.thing_id 
+        SELECT t.thing_id
         FROM things t
-        ORDER BY RANDOM() * t.significance DESC
+        ORDER BY RANDOM() * LOG(t.significance + 1)
+ DESC
         LIMIT 1;
         """)
     if result:
         return result["thing_id"]
+    return None
 
 
 async def get_thing(thing_id: EntityId) -> Thing | None:
-    result = await g.connection.fetch_one("""
+    result: Thing = await g.connection.fetch_one(
+        """
     SELECT thing_id, label, description, image_url FROM things WHERE thing_id = :thing_id
-    """, {"thing_id": thing_id}
+    """,
+        {"thing_id": thing_id},
     )
     if result:
         return Thing(**result)
+    return None
 
 
-@app.get('/<string:thing_id>')
+@app.get("/<string:thing_id>")
 async def thing_view(thing_id: EntityId):
     thing = await get_thing(thing_id)
 
@@ -115,10 +133,10 @@ async def thing_view(thing_id: EntityId):
     if request.headers.get("HX-Request"):
         return await render_block("thing.html", "content", thing=thing)
 
-    return await render_template("thing.html", thing=thing, session=session.get('user'))
+    return await render_template("thing.html", thing=thing, session=session.get("user"))
 
 
-@app.post('/<string:thing_id>/like')
+@app.post("/<string:thing_id>/like")
 async def vote_like(thing_id: EntityId):
     user_id = pydash.get(session, "user.userinfo.sub")
     if not user_id:
@@ -139,13 +157,15 @@ async def vote_like(thing_id: EntityId):
 
     if request.headers.get("HX-Request"):
         response = await make_response()
-        response.headers["HX-Location"] = json.dumps({"path": f"/{new_thing}", "target": "main"})
+        response.headers["HX-Location"] = json.dumps(
+            {"path": f"/{new_thing}", "target": "main"}
+        )
         return response
 
     return redirect(f"/{new_thing}")
 
 
-@app.post('/<string:thing_id>/skip')
+@app.post("/<string:thing_id>/skip")
 async def vote_skip(thing_id: EntityId):
     user_id = pydash.get(session, "user.userinfo.sub")
     if not user_id:
@@ -166,7 +186,9 @@ async def vote_skip(thing_id: EntityId):
 
     if request.headers.get("HX-Request"):
         response = await make_response()
-        response.headers["HX-Location"] = json.dumps({"path": f"/{new_thing}", "target": "main"})
+        response.headers["HX-Location"] = json.dumps(
+            {"path": f"/{new_thing}", "target": "main"}
+        )
         return response
 
     return redirect(f"/{new_thing}")
